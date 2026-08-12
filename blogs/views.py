429ed3730b26a91b5b65bc155db05f8240445f4e
ajayishwarakatti26 <ajayishwarakatti26@gmail.com>
2026-08-12 11:03:18 +0530

    creat the base.html

--- blogs/views.py
+++ blogs/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import Blog
+from .models import Blog,Category
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
 def posts_by_category(request,category_id):
     posts=Blog.objects.filter(status="1", category=category_id)
+    try:
+         category=Category.objects.get(pk=category_id)
+    except:
+         return redirect('home')
+         
     context={
         'posts':posts,
+        'category':category,
+        
     }
     return render(request,"posts_by_category.html",context)
     
